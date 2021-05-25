4635422d2b78502d05fca7ffa87302765a493b2a
slavaider <slavaider1@gmail.com>
2021-05-25 22:44:29 +0500

    fix: fix eslint

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 // Routes
+import path from 'path';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/board/board.router';
 import taskRouter from './resources/task/task.router';
-import path from 'path';
+
 
 const app = express();
 const swaggerDocument = YAML.load(path.resolve(__dirname, '../doc/api.yaml'));
